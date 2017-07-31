ced9abf05ecf2828220fe17f6ad8bcca0f092250
James Riady <jamesriady1998@yahoo.com>
2017-07-31 21:55:40 +0700

    make Definition in Category

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,39 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *	definition="category",
+ *	@SWG\Property(
+ *   	property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *   	property="name",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *   	property="icon",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *   	property="category",
+ *      type="boolean",
+ *		default=true
+ *  ),
+ *	@SWG\Property(
+ *   	property="create_at",
+ *       type="string"
+ *  ),
+ *	@SWG\Property(
+ *   	property="update_at",
+ *       type="string"
+ *  )
+ * )
+ */
+
 class Category extends Model
 {
-    protected $fillable = ['nama', 'icon', 'category'];
+    protected $fillable = ['name', 'icon', 'category'];
 }
