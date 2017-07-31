2c0f0000fb06700599534cd4472bb05c04eb4e6b
James Riady <jamesriady1998@yahoo.com>
2017-07-31 21:58:28 +0700

    setting swagger in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,35 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+	/**
+     * @SWG\Swagger(
+     *   basePath="",
+     *   host="crunchy-api.dev",
+     *   schemes={"http"},
+     *   @SWG\Info(
+     *       version="1.0",
+     *       title="Crunchy API",
+     *       @SWG\Contact(
+     *           name="SmallCorridor",
+     *           url="https://uph.edu"
+     *       ),
+     *   ),
+     *   @SWG\Definition(
+     *       definition="Error",
+     *       required={"code", "message"},
+     *       @SWG\Property(
+     *           property="code",
+     *           type="integer",
+     *           format="int32"
+     *       ),
+     *       @SWG\Property(
+     *           property="message",
+     *           type="string"
+     *       )
+     *   )
+     * )
+     */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
