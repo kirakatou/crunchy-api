0dc84fc59b21cd3c042e16533b54ec110d36723f
James Riady <jamesriady1998@yahoo.com>
2017-08-01 12:30:17 +0700

    add Definition in Comment and Post model

--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,58 @@ namespace App;
 use Storage;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *  definition="post",
+ *  @SWG\Property(
+ *      property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *      property="user_id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *      property="merchant_id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *      property="path",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="description",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="taste",
+ *      type="number",
+ *      format="double"
+ *  ),
+ *  @SWG\Property(
+ *      property="price",
+ *      type="number",
+ *      format="double"
+ *  ),
+ *  @SWG\Property(
+ *      property="service",
+ *      type="number",
+ *      format="double"
+ *  ),
+ *  @SWG\Property(
+ *      property="create_at",
+ *       type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="update_at",
+ *       type="string"
+ *  )
+ * )
+ */
+
 class Post extends Model
 {
     public function user() {
