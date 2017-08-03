41ada6eeed820ea7d9acbda2b90f345adf4a4518
James Riady <jamesriady1998@yahoo.com>
2017-08-03 21:44:43 +0700

    add migration for user followers

---
+++ database/migrations/2017_08_03_131740_create_user_followers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_followers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                  ->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('follower_id')->unsigned();
+            $table->foreign('follower_id')->references('id')->on('users')
+                  ->onUpdate('cascade')->onDelete('cascade'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_followers');
+    }
+}
