77f0a39ab16f832adf08edf86d7ac713e20e09b1
mario yaputra <mario yaputra>
2017-08-04 01:25:24 +0700

    add documentation for Coupon

--- app/Coupon.php
+++ app/Coupon.php
@@ -4,7 +4,36 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *	definition="coupon",
+ *	@SWG\Property(
+ *   	property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *   	property="coupon_title",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *   	property="point",
+ *      type="integer",
+ *		format="int32"
+ *  ),
+ *  @SWG\Property(
+ *   	property="description",
+ *      type="text"
+ *  ),
+ *  @SWG\Property(
+ *   	property="amount",
+ *      type="integer",
+ *      format="int32"
+ *  )
+ * )
+ */
+
 class Coupon extends Model
 {
-    protected $fillable = ['coupon_title', 'point', 'description'];
+    protected $fillable = ['coupon_title', 'point', 'description','amount'];
 }
