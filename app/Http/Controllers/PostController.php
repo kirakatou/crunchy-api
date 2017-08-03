5efbc2b50d488bbeb7bf2f7e3eac4f85aba663a3
Sutandi Tan <sutandii@icloud.com>
2017-08-03 14:12:16 +0700

    show function on post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -106,7 +106,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::findOrFail($id);
+        return $post;
     }
 
     /**
