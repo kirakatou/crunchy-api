33c587d149694990a4defee654a2f8afe7243e75
mario yaputra <mario yaputra>
2017-08-04 01:47:01 +0700

    add documentation for ReportList

--- app/ReportList.php
+++ app/ReportList.php
@@ -4,6 +4,32 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *	definition="reportlist",
+ *	@SWG\Property(
+ *   	property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *	@SWG\Property(
+ *   	property="user_id",
+ *      type="integer",
+ *      format="int32"
+ *  )
+ *	@SWG\Property(
+ *   	property="post_id",
+ *      type="integer",
+ *      format="int32"
+ *  )
+ *	@SWG\Property(
+ *   	property="report_id",
+ *      type="integer",
+ *      format="int32"
+ *  )
+ * )
+ */
+
 class ReportList extends Model
 {
     protected $fillable = ['user_id', 'post_id', 'report_id'];
