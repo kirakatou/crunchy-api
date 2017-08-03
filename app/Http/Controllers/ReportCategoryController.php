e4099e92d15c3c7a511b51edc4a20cc5ea645e9e
mario yaputra <mario yaputra>
2017-08-04 01:43:32 +0700

    add documentation for ReportCategoryController

--- app/Http/Controllers/ReportCategoryController.php
+++ app/Http/Controllers/ReportCategoryController.php
@@ -13,6 +13,36 @@ class ReportCategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Get(
+     *      path="/api/v1/food/reportcategory",
+     *      summary="Retrieves the collection of Report Category resources.",
+     *      produces={"application/json"},
+     *      tags={"ReportCategory"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="Report Categories collection.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/reportcategory")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer" 
+     *      )    
+     * )
+     */
+
     public function index()
     {
         $reports = ReportCategory::paginate();
@@ -35,6 +65,50 @@ class ReportCategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Post(
+     *      path="/api/v1/food/reportcategory",
+     *      summary="Add new Report Category.",
+     *      produces={"application/json"},
+     *      consumes={"application/json"},
+     *      tags={"ReportCategory"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="new Report Category has successfully added.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/reportcategory")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer",
+     *      ),
+     *       @SWG\Parameter(
+     *           name="body",
+     *           in="body",
+     *           required=true,
+     *           description="Report Category object that needs to be added to the database", 
+     *           type="string",
+     *           @SWG\Schema(
+     *               @SWG\Property(
+     *                   property="name",
+     *                   type="string"
+     *               )   
+     *           )
+     *      )              
+     * )
+     */
+
     public function store(Request $request)
     {
         $report = new ReportCategory();
@@ -49,9 +123,58 @@ class ReportCategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Get(
+     *      path="/api/v1/food/reportcategory/{id}",
+     *      summary="Find Report Category by ID.",
+     *      produces={"application/json"},
+     *      tags={"ReportCategory"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="This is the data that you search.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/reportcategory")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=400,
+     *          description="Invalid ID."
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Response(
+     *          response=404,
+     *          description="Report Category not found."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer",
+     *      ),
+     *      @SWG\Parameter(
+     *           name="id",
+     *           in="path",
+     *           description="Please enter the reportcategoryId",
+     *           required=true, 
+     *           type="integer"
+     *      )              
+     * )
+     */
+
     public function show($id)
     {
-        //
+        $report = ReportCategory::findOrFail($id);
+        if(empty($report)){
+            return response()->json(['message' => 'Report Category ID not found'], 404);
+        }
+        return response()->json($report);
     }
 
     /**
@@ -72,6 +195,65 @@ class ReportCategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Put(
+     *      path="/api/v1/food/reportcategory/{id}",
+     *      summary="Update the Report Category resource.",
+     *      produces={"application/json"},
+     *      consumes={"application/json"},
+     *      tags={"ReportCategory"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="new Report Category has successfully updated.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/reportcategory")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=400,
+     *          description="Invalid ID."
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Response(
+     *          response=404,
+     *          description="Report Category not found."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer",
+     *      ),
+     *      @SWG\Parameter(
+     *           name="id",
+     *           in="path",
+     *           description="Please enter the categoryreportId",
+     *           required=true, 
+     *           type="integer"
+     *      ),
+     *      @SWG\Parameter(
+     *           name="body",
+     *           in="body",
+     *           required=true,
+     *           description="Report Category object that needs to be added to the database", 
+     *           type="string",
+     *           @SWG\Schema(
+     *               @SWG\Property(
+     *                   property="name",
+     *                   type="string"
+     *               )   
+     *           )
+     *      )                
+     * )
+     */
+
     public function update(Request $request, $id)
     {
         $report = ReportCategory::findOrFail($id);
@@ -86,6 +268,43 @@ class ReportCategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Delete(
+     *      path="/api/v1/food/reportcategory/{id}",
+     *      summary="Remove the Report Category resource.",
+     *      produces={"application/json"},
+     *      tags={"ReportCategory"},
+     *      @SWG\Response(
+     *          response=204,
+     *          description="Report Category resource deleted."
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Response(
+     *          response=404,
+     *          description="Report Category not found."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer",
+     *      ),
+     *      @SWG\Parameter(
+     *           name="id",
+     *           in="path",
+     *           description="Please enter the reportcategoryId",
+     *           required=true, 
+     *           type="integer"
+     *      )              
+     * )
+     */
+
     public function destroy($id)
     {
         $report = ReportCategory::findOrFail($id);
