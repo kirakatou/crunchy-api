3fedd2b27be65a4e97fdd00c85ebd1f3c01ed748
mario yaputra <mario yaputra>
2017-08-04 01:28:59 +0700

    add documentation for ReportCategory

--- app/ReportCategory.php
+++ app/ReportCategory.php
@@ -4,6 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *	definition="ReportCategory",
+ *	@SWG\Property(
+ *   	property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *   	property="name",
+ *      type="string"
+ *  )
+ * )
+ */
+
 class ReportCategory extends Model
 {
     protected $fillable = [
