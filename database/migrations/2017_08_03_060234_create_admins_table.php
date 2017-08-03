7cb5be9779b0c5c9073a09e1295cefcb4e70ab33
Sutandi Tan <sutandii@icloud.com>
2017-08-03 21:20:47 +0700

    make migration for admin

---
+++ database/migrations/2017_08_03_060234_create_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->boolean('gender');
+            $table->text('address')->nullable();
+            $table->string('phone_no')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
