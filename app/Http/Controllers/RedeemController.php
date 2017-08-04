7c0f65e45ac9af5fc4ce89d78d5a310493a7dccc
mario yaputra <mario yaputra>
2017-08-04 18:55:30 +0700

    add documentation for RedeemController

--- app/Http/Controllers/RedeemController.php
+++ app/Http/Controllers/RedeemController.php
@@ -14,6 +14,36 @@ class RedeemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Get(
+     *      path="/api/v1/redeem",
+     *      summary="Retrieves the collection of Coupon resources for redeem.",
+     *      produces={"application/json"},
+     *      tags={"Redeem"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="Coupons collection for redeem.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/coupon")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer" 
+     *      )    
+     * )
+     */
+
     public function index()
     {
         $coupons = Coupon::paginate();
@@ -36,6 +66,44 @@ class RedeemController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Post(
+     *      path="/api/v1/redeem/{coupon_id}",
+     *      summary="Redeem a coupon.",
+     *      produces={"application/json"},
+     *      consumes={"application/json"},
+     *      tags={"Redeem"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="coupon *coupon's name* redeemed.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/coupon")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer",
+     *      ),
+     *      @SWG\Parameter(
+     *           name="coupon_id",
+     *           in="path",
+     *           description="Please enter the couponId",
+     *           required=true, 
+     *           type="integer"
+     *      )              
+     * )
+     */
+
     public function store(Request $request, $id)
     {
         $coupon = Coupon::findOrFail($id);
@@ -61,9 +129,58 @@ class RedeemController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @SWG\Get(
+     *      path="/api/v1/redeem/{coupon_id}",
+     *      summary="Find Coupon by ID.",
+     *      produces={"application/json"},
+     *      tags={"Redeem"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="This is the data that you search.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/coupon")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=400,
+     *          description="Invalid ID."
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Response(
+     *          response=404,
+     *          description="Coupon not found."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer",
+     *      ),
+     *      @SWG\Parameter(
+     *           name="coupon_id",
+     *           in="path",
+     *           description="Please enter the couponId",
+     *           required=true, 
+     *           type="integer"
+     *      )              
+     * )
+     */
+
     public function show($id)
     {
-        //
+        $coupon = Coupon::findOrFail($id);
+        if(empty($coupon)){
+            return response()->json(['message' => 'Coupon ID not found'], 404);
+        }
+        return response()->json($coupon);
     }
 
     /**
