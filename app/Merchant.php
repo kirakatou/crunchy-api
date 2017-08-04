14974f238789754ca6dcefee3a311dc895133921
James Riady <jamesriady1998@yahoo.com>
2017-08-04 11:57:29 +0700

    add definition for merchant

--- app/Merchant.php
+++ app/Merchant.php
@@ -4,6 +4,37 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *	definition="merchant",
+ *	@SWG\Property(
+ *   	property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *   	property="name",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *   	property="address",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *   	property="phone_no",
+ *      type="string"
+ *  ),
+ *	@SWG\Property(
+ *   	property="create_at",
+ *       type="string"
+ *  ),
+ *	@SWG\Property(
+ *   	property="update_at",
+ *       type="string"
+ *  )
+ * )
+ */
+
 class Merchant extends Model
 {
     public function post() {
