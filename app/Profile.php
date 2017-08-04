c085e4f3af42054543c352dbceb583036233c902
James Riady <jamesriady1998@yahoo.com>
2017-08-04 11:41:35 +0700

    add Definition in profile and user follower

--- app/Profile.php
+++ app/Profile.php
@@ -5,6 +5,54 @@ namespace App;
 use App\Title;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *  definition="profile",
+ *  @SWG\Property(
+ *      property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *      property="user_id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *      property="name",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="email",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="gender",
+ *      type="boolean"
+ *  ),
+ *  @SWG\Property(
+ *      property="photo",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="max_exp",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="title",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="create_at",
+ *       type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="update_at",
+ *       type="string"
+ *  )
+ * )
+ */
+
 class Profile extends Model
 {
 	protected $fillable = [
