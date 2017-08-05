9c8c318e75d1980001c18199c58afeef61209732
James Riady <jamesriady1998@yahoo.com>
2017-08-06 00:16:05 +0700

    add definition for admin

--- app/Admin.php
+++ app/Admin.php
@@ -4,6 +4,44 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @SWG\Definition(
+ *  definition="admin",
+ *  @SWG\Property(
+ *      property="id",
+ *      type="integer",
+ *      format="int32"
+ *  ),
+ *  @SWG\Property(
+ *      property="name",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="email",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="gender",
+ *      type="boolean"
+ *  ),
+ *  @SWG\Property(
+ *      property="address",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="phone_no",
+ *      type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="create_at",
+ *       type="string"
+ *  ),
+ *  @SWG\Property(
+ *      property="update_at",
+ *       type="string"
+ *  )
+ * )
+ */
 class Admin extends Model
 {
     public function user() {
