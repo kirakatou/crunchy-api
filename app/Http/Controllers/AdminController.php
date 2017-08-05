d4f54daf639e78271b332570453e8491bbda1acc
James Riady <jamesriady1998@yahoo.com>
2017-08-06 00:16:45 +0700

    add documentation in AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,6 +13,34 @@ class AdminController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    /**
+     * @SWG\Get(
+     *      path="/api/v1/food/admin",
+     *      summary="Retrieves the collection of Admin resources.",
+     *      produces={"application/json"},
+     *      tags={"Admin"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="Admins collection.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/admin")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer" 
+     *      )    
+     * )
+     */
     public function index()
     {
         $admins = Admin::with("user")->paginate();
@@ -35,6 +63,66 @@ class AdminController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+      /**
+        * @SWG\Post(
+        *   path="/api/v1/food/admin",
+        *   summary="Register new Admin.",
+        *   produces={"application/json"},
+        *   consumes={"application/json"},
+        *   tags={"register"},
+        *       @SWG\Response(
+        *           response=200,
+        *           description="Admin token.",
+        *           @SWG\Schema(
+        *              type="array",
+        *              @SWG\Items(ref="#/definitions/admin")
+        *          )
+        *       ),
+        *       @SWG\Response(
+        *           response=401,
+        *           description="Unauthorized action."
+        *       ),
+        *       @SWG\Parameter(
+        *           name="body",
+        *           in="body",
+        *           required=true,
+        *           description="Haven't an account? Register here!",
+        *           type="string",
+        *           @SWG\Schema(
+        *               @SWG\Property(
+        *                   property="username",
+        *                   type="string"
+        *               ),
+        *               @SWG\Property(
+        *                   property="name",
+        *                   type="string"
+        *               ),
+        *               @SWG\Property(
+        *                   property="password",
+        *                   type="string",
+        *                   format="password"
+        *               ),
+        *               @SWG\Property(
+        *                   property="email",
+        *                   type="string"
+        *               ),   
+        *               @SWG\Property(
+        *                   property="gender",
+        *                   type="boolean"
+        *               ),
+        *               @SWG\Property(
+        *                   property="address",
+        *                   type="string"
+        *               ),
+        *               @SWG\Property(
+        *                   property="phone_no",
+        *                   type="string"
+        *               ),
+        *           )
+        *      )
+        * )
+        */ 
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -65,6 +153,41 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    /**
+     * @SWG\Get(
+     *      path="/api/v1/food/{adminId}",
+     *      summary="Retrieves the collection of Admin resources.",
+     *      produces={"application/json"},
+     *      tags={"Admin"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="Admins collection.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/admin")
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=401,
+     *          description="Unauthorized action."
+     *      ),
+     *      @SWG\Parameter(
+     *          name="Authorization",
+     *          description="Example = Bearer(space)'your_token'",
+     *          in="header",
+     *          required=true,
+     *          type="string",
+     *          default="Bearer" 
+     *      ),
+     *      @SWG\Parameter(
+     *          name="adminId",
+     *          in="path",
+     *          description="Please enter the adminId",
+     *          required=true,
+     *          type="integer"
+     *      )    
+     * )
+     */
     public function show($id)
     {
         $admin = Admin::findOrFail($id);
@@ -90,6 +213,81 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+      /**
+        * @SWG\Put(
+        *   path="/api/v1/food/{adminId}",
+        *   summary="Update admin.",
+        *   produces={"application/json"},
+        *   consumes={"application/json"},
+        *   tags={"Admin"},
+        *       @SWG\Response(
+        *           response=200,
+        *           description="Admin updated.",
+        *          @SWG\Schema(
+        *              type="array",
+        *              @SWG\Items(ref="#/definitions/admin")
+        *          )
+        *       ),
+        *       @SWG\Response(
+        *           response=401,
+        *           description="Unauthorized action."
+        *       ),
+        *      @SWG\Parameter(
+        *          name="Authorization",
+        *          description="Example = Bearer(space)'your_token'",
+        *          in="header",
+        *          required=true,
+        *          type="string",
+        *          default="Bearer" 
+        *      ),
+        *      @SWG\Parameter(
+        *           name="adminId",
+        *           in="path",
+        *           description="Please enter the adminId",
+        *           required=true, 
+        *           type="integer"
+        *      ),
+        *      @SWG\Parameter(
+        *           name="body",
+        *           in="body",
+        *           required=true,
+        *           description="Update your account here!",
+        *           type="string",
+        *           @SWG\Schema(
+        *               @SWG\Property(
+        *                   property="username",
+        *                   type="string"
+        *               ),
+        *               @SWG\Property(
+        *                   property="name",
+        *                   type="string"
+        *               ),
+        *               @SWG\Property(
+        *                   property="password",
+        *                   type="string",
+        *                   format="password"
+        *               ),
+        *               @SWG\Property(
+        *                   property="email",
+        *                   type="string"
+        *               ),   
+        *               @SWG\Property(
+        *                   property="gender",
+        *                   type="boolean"
+        *               ),
+        *               @SWG\Property(
+        *                   property="address",
+        *                   type="string"
+        *               ),
+        *               @SWG\Property(
+        *                   property="phone_no",
+        *                   type="string"
+        *               )
+        *           )
+        *      )
+        * )
+        */ 
     public function update(Request $request, $id)
     {
         $admin = Admin::findOrFail($id);
